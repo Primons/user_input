72f6c6561b2d22dc4e092bef5966efdc68a2e82d
Priscillah Mogesi <mogesipriscillah7@gmail.com>
2024-02-23 10:23:50 +0300

    Add files via upload

---
+++ user_input.py
@@ -0,0 +1,7 @@
+name = input("Enter first name: ")
+age = input("How old are you?: ")
+location = input("Your location: ")
+
+#print personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
